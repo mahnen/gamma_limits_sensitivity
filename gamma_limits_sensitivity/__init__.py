7a32a7fce4bee8045cb11239d8a3b5284d6cf6a7
Max Ludwig Ahnen <m.knoetig@gmail.com>
2016-11-10 12:34:02 +0100

    more api defined

--- gamma_limits_sensitivity/__init__.py
+++ gamma_limits_sensitivity/__init__.py
@@ -8,3 +8,20 @@ import matplotlib.pyplot as plt
 def upper_limit(on, off, alpha, l_lim, A_eff):
     figure = plt.figure()
     return [figure]
+
+
+def sensitivity(s_bg, alpha, t_obs, A_eff):
+    figure = plt.figure()
+    return [figure]
+
+
+def predict(s_bg, alpha, f_0, df_0, Gamma, dGamma, E0, A_eff):
+    figures = [plt.figure()]
+    times = [1., 2., 3.]
+
+    dictionary = {
+        'times': times,
+        'plots': figures
+        }
+
+    return dictionary
--- gamma_limits_sensitivity/tests/test_high_level_api.py
+++ gamma_limits_sensitivity/tests/test_high_level_api.py
@@ -11,18 +11,18 @@ she calls it as explained:
 
    gamma_foo_bar UL --on=10 --off=50 --alpha=0.2 --l_lim=15 --A_eff=<some_path>
 
-and some nice plots pop up!
+and some nice plots return
 
 --------------------------------------------------------------
 
-A sunny day ... her boss is really interessted in knowing what her newly developed
-telescope is actually capable of, independend of the source.
+A sunny day ... her boss is really interessted in knowing what
+her newly developed telescope is actually capable of, independend of the source
 
 So she calls:
 
-    gamma_foo_bar SENSE --s_bg=7.1 --alpha=0.2 --A_eff=<some_path> --t_obs=36000
+    gamma_foo_bar SENS --s_bg=7.1 --alpha=0.2 --t_obs=36000 --A_eff=<some_path>
 
-and gets one plot
+and gets plots
 
 --------------------------------------------------------------
 
@@ -32,7 +32,8 @@ reasonable amount of time
 
 so she calls:
 
-    gamma_foo_bar PREDICT --s_bg=7.1 --alpha=0.2 --f0=1e-12 --df0=1e-13 --Gamma=-2.6 --dGamma=0.2 --E0=1e9 --A_eff=<some_path>
+    gamma_foo_bar PREDICT --s_bg=7.1 --alpha=0.2 --f0=1e-12 --df0=1e-13
+        --Gamma=-2.6 --dGamma=0.2 --E0=1e9 --A_eff=<some_path>
 
 and gets some plots again and the time to detection  printed to stdout.
 
@@ -44,7 +45,8 @@ import matplotlib
 
 def test_high_level_api_ul():
     '''
-    This test tests the cli functionality explained in above user story one.
+    This test tests the cli upper limit functionality explained in above user
+    story.
 
     '''
     plots = gls.upper_limit(
@@ -58,3 +60,42 @@ def test_high_level_api_ul():
     for plot in plots:
         assert isinstance(plot, matplotlib.figure.Figure)
 
+
+def test_high_level_api_sens():
+    '''
+    This test tests the cli sens functionality explained in above user story.
+
+    '''
+    plots = gls.sensitivity(
+        s_bg=10,
+        alpha=0.2,
+        t_obs=10*3600,
+        A_eff='some_path',
+        )
+
+    for plot in plots:
+        assert isinstance(plot, matplotlib.figure.Figure)
+
+
+def test_high_level_api_predict():
+    '''
+    This test tests the cli predict functionality explained
+    in above user story.
+
+    '''
+    dictionary = gls.predict(
+        s_bg=10,
+        alpha=0.2,
+        f_0=1e-12,
+        df_0=1e-13,
+        Gamma=-2.6,
+        dGamma=0.2,
+        E0=1e9,
+        A_eff='some_path',
+        )
+
+    for plot in dictionary['plots']:
+        assert isinstance(plot, matplotlib.figure.Figure)
+
+    for time_quantile in dictionary['times']:
+        assert time_quantile >= 0
